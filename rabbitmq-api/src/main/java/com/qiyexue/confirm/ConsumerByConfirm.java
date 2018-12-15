7db8d9589f4e3286e3a90b2dda0e3e6df6eff666
qiyexue <564660143@qq.com>
2018-12-15 16:33:21 +0800

    Confirm消息确认

---
+++ rabbitmq-api/src/main/java/com/qiyexue/confirm/ConsumerByConfirm.java
@@ -0,0 +1,56 @@
+package com.qiyexue.confirm;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+/**
+ * Confirm消息确认的生产者
+ *
+ * @author 七夜雪
+ * @create 2018-12-15 16:19
+ */
+public class ConsumerByConfirm {
+
+    public static void main(String[] args) throws Exception {
+        // 1. 创建连接工厂并设置属性
+        ConnectionFactory factory = new ConnectionFactory();;
+        factory.setHost("192.168.72.138");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+
+        // 2. 创建连接
+        Connection connection = factory.newConnection();
+
+        // 3. 创建channel
+        Channel channel = connection.createChannel();
+
+        // 4. 声明Exchange
+        String exchangeName = "test_confirm_exchange";
+        String exchangeType = "topic";
+        String routingKey = "confirm.*";
+        channel.exchangeDeclare(exchangeName, exchangeType, true, false, null);
+
+        // 5. 声明消息队列
+        String queueName = "test_confirm_queue";
+        channel.queueDeclare(queueName, true, false, false, null);
+
+        // 6. 绑定队列和Exchange
+        channel.queueBind(queueName, exchangeName, routingKey);
+
+        // 7. 创建一个消费者
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+
+        // 8. 设置消费者从哪个队列开始消费, 设置自动ACK
+        channel.basicConsume(queueName, true, consumer);
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String msg = new String(delivery.getBody());
+            System.out.println(msg);
+        }
+
+    }
+
+}
---
+++ rabbitmq-api/src/main/java/com/qiyexue/confirm/ProducerByConfirm.java
@@ -0,0 +1,55 @@
+package com.qiyexue.confirm;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+
+/**
+ * Confirm确认消息模式生产者
+ *
+ * @author 七夜雪
+ * @create 2018-12-15 16:12
+ */
+public class ProducerByConfirm {
+
+    public static void main(String[] args) throws Exception {
+        // 1. 创建ConnectionFactory, 并设置属性
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.72.138");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+
+        // 2. 创建连接
+        Connection connection = factory.newConnection();
+
+        // 3. 创建channel
+        Channel channel = connection.createChannel();
+
+        // 4. 开启Confirm模式
+        channel.confirmSelect();
+
+        String exchangeName = "test_confirm_exchange";
+        String routingKey = "confirm.qiye";
+
+        // 5. 发送消息
+        String msg = "Send Msg By Confirm ...";
+        channel.basicPublish(exchangeName, routingKey, null, msg.getBytes());
+
+        // 6. 设置监听
+        channel.addConfirmListener(new ConfirmListener() {
+            public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+                System.out.println("-------ACK Success--------");
+            }
+
+            public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+                System.err.println("-------ACK Failed--------");
+            }
+        });
+
+        // 因为设置了监听, 这里就不关闭channel和connection了
+    }
+
+}
