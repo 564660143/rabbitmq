543d2b013ba3fb9f17c1c39ac1de5c0ac23aef30
qiyexue <564660143@qq.com>
2018-12-15 15:55:01 +0800

    消息属性设置

---
+++ rabbitmq-api/src/main/java/com/qiyexue/message/Consumer.java
@@ -0,0 +1,49 @@
+package com.qiyexue.message;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
+
+/**
+ * 消费者
+ *
+ * @author 七夜雪
+ * @create 2018-12-13 20:57
+ */
+public class Consumer {
+
+    public static void main(String[] args) throws Exception {
+        // 1. 创建连接工厂, 设置属性
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.72.138");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+
+        // 2. 创建连接
+        Connection connection = factory.newConnection();
+
+        // 3. 使用connection创建channel
+        Channel channel = connection.createChannel();
+
+        // 4. 声明(创建)一个队列
+        String queueName = "test01";
+        channel.queueDeclare(queueName,true, false, false, null);
+
+        // 5. 创建消费者
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+
+        // 6. 设置channel
+        channel.basicConsume(queueName, true, consumer);
+        while (true) {
+            // 7. 获取消息
+            Delivery delivery = consumer.nextDelivery();
+            System.out.println(new String(delivery.getBody()));
+            // 获取head中内容
+            System.out.println(delivery.getProperties().getHeaders().get("name"));
+        }
+        
+    }
+
+}
---
+++ rabbitmq-api/src/main/java/com/qiyexue/message/Producer.java
@@ -0,0 +1,55 @@
+package com.qiyexue.message;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 生产者
+ *
+ * @author 七夜雪
+ * @create 2018-12-13 20:43
+ */
+public class Producer {
+
+    public static void main(String[] args) throws Exception {
+        // 1. 创建连接工厂, 设置属性
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.72.138");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+
+        // 2. 创建连接
+        Connection connection = factory.newConnection();
+
+        // 3. 使用connection创建channel
+        Channel channel = connection.createChannel();
+
+        // 4. 通过channel发送消息
+        String msg = "hello rabbitmq!";
+        AMQP.BasicProperties properties = new AMQP.BasicProperties();
+        Map<String,Object> headers = new HashMap<String, Object>();
+        headers.put("name", "七夜雪");
+        properties = properties.builder()
+                // 设置编码为UTF8
+                .contentEncoding("UTF-8")
+                // 设置自定义Header
+                .headers(headers)
+                // 设置消息失效时间
+                .expiration("5000").build();
+
+        for (int i = 0; i < 5; i++) {
+            // 不指定exchange的情况下, 使用默认的exchange, routingKey与队列名相等
+            channel.basicPublish("", "test01", properties, msg.getBytes());
+        }
+
+        // 5. 关闭连接
+        channel.close();
+        connection.close();
+    }
+
+}
